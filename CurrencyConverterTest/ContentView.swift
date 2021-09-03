70f7aba20f5cd3ace05d118d9e3397fa4d7f22fe
Justas Serstkovas <notPublic@willNotProvide.Com>
2021-09-03 12:38:51 +0300

    test push

---
+++ CurrencyConverterTest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CurrencyConverterTest
+//
+//  Created by Justas Serstkovas on 2021-09-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+//test push
---
+++ CurrencyConverterTest/CurrencyConverterTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterTestApp.swift
+//  CurrencyConverterTest
+//
+//  Created by Justas Serstkovas on 2021-09-03.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
